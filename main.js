ab72a20d96e715b26421a2accbfa4385aeedd40f
Watcharaponlee <watcharapon.lee@gmail.com>
2024-07-25 13:41:16 +0700

    Update_25/7/2567
    Add
    Certificate - { image slider }
    Footer

--- main.js
+++ main.js
@@ -10,3 +10,31 @@ window.addEventListener('scroll', function() {
         backToTopButton.classList.remove('show');
     }
 });
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+    // เช็คข้อผิดพลาดของ index
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+setInterval(nextSlide, 5000);
